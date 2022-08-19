f8a82388cfa47c6434dfa17b32f1e80d071d9174
Dan Shawl <danielrshawl@gmail.com>
2022-08-18 22:18:27 -0400

    buildTower

--- js/6kyu/buildTower.js
+++ js/6kyu/buildTower.js
@@ -17,3 +17,24 @@
 //   " ********* ",
 //   "***********"
 // ]
+
+function towerBuilder(nFloors) {
+  //  convert to an array
+  //  get prime numbers
+  const floors = []
+  // let space = 
+  let prime = 1;
+
+  floors.map((floor) => {
+    floor.push(`${}`)
+  })
+
+  for (let i = 0; i < nFloors - 1; i++) {
+    // console.log(prime);
+    prime += 2;
+    console.log(prime);
+  }
+}
+
+// towerBuilder(3)
+console.log(towerBuilder(6));
