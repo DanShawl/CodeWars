99849040a941d73a598080652e2bddc9224a4884
Dan Shawl <danielrshawl@gmail.com>
2022-08-23 13:43:33 -0400

    generateHashtag

--- js/6kyu/buildTower.js
+++ js/6kyu/buildTower.js
@@ -19,22 +19,42 @@
 // ]
 
 function towerBuilder(nFloors) {
-  //  convert to an array
-  //  get prime numbers
-  const floors = []
-  // let space = 
+  const floors = [];
+  let newFloors = [];
   let prime = 1;
 
-  floors.map((floor) => {
-    floor.push(`${}`)
-  })
-
-  for (let i = 0; i < nFloors - 1; i++) {
-    // console.log(prime);
+  for (let j = 0; j < nFloors; j++) {
+    floors.push(`${character.repeat(prime)}`);
     prime += 2;
-    console.log(prime);
   }
+
+  console.log(floors);
+  console.log(newFloors);
 }
 
-// towerBuilder(3)
-console.log(towerBuilder(6));
+towerBuilder(6);
+// console.log(towerBuilder(6));
+
+// if (j !== nFloors) {
+//   floors.map((floor) => {
+//     console.log(` ${floor} `);
+//   });
+// }
+// console.log(floors);
+
+function towerBuilder(n) {
+  return Array.from({ length: n }, function (v, k) {
+    const spaces = ' '.repeat(n - k - 1);
+    return spaces + '*'.repeat(k + k + 1) + spaces;
+  });
+}
+
+// function towerBuilder(nFloors) {
+//   var tower = [];
+//   for (var i = 0; i < nFloors; i++) {
+//     tower.push(" ".repeat(nFloors - i - 1)
+//              + "*".repeat((i * 2)+ 1)
+//              + " ".repeat(nFloors - i - 1));
+//   }
+//   return tower;
+// }
