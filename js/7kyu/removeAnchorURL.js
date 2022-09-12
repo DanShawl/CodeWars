ce6875967060384370f0a1d19dded5670e6fb688
Dan Shawl <danielrshawl@gmail.com>
2022-09-12 10:30:53 -0400

    removeAnchorURL

--- js/7kyu/removeAnchorURL.js
+++ js/7kyu/removeAnchorURL.js
@@ -6,10 +6,10 @@
 
 function removeUrlAnchor(url) {
   url = url.split('');
-  url.length = url.indexOf('#') ? url.indexOf('#') : url.length;
-  url = url.join('');
-  return url;
+  url.length = url.includes('#') ? url.indexOf('#') : url.length;
+  return url.join('');
 }
 
 console.log(removeUrlAnchor('www.codewars.com#about'));
 console.log(removeUrlAnchor('www.codewars.com/katas/?page=1#about'));
+console.log(removeUrlAnchor('www.codewars.com/katas/'));
