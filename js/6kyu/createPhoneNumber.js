961bff888c431111689d83c857a5dc0e4c42aba6
Dan Shawl <danielrshawl@gmail.com>
2022-10-04 23:58:43 -0400

    createPhoneNumber

--- js/6kyu/createPhoneNumber.js
+++ js/6kyu/createPhoneNumber.js
@@ -1,6 +1,14 @@
 // Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
 // "(123) 456-7890"
 
+const phone = (numbers) => {
+  let format = '(xxx) xxx-xxxx';
+  numbers.forEach((number) => (format = format.replace('x', number)));
+  console.log(format);
+};
+
+phone([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
+
 //  My solution
 function createPhoneNumber(numbers) {
   const [one, two, three, four, five, six, seven, eight, nine, ten] = [
@@ -21,4 +29,4 @@ function createPhoneNumber(numbers) {
 //   return format;
 // }
 
-createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
+// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]);
