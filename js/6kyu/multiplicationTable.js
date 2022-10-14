38feac454fda5ce86d82e3f918e84bbbd0dd0827
Dan Shawl <danielrshawl@gmail.com>
2022-10-13 23:01:57 -0400

    multiplicationTable

--- js/6kyu/multiplicationTable.js
+++ js/6kyu/multiplicationTable.js
@@ -8,5 +8,26 @@
 // for given example, the return value should be: [[1,2,3],[2,4,6],[3,6,9]]
 
 multiplicationTable = function (size) {
-  // insert code here
+  //  item = arr[0][i] * (i + 1)
+  //  push item to new array
+  //  repeat for size n
+
+  let arr = [[]];
+  for (let i = 0; i < size; i++) {
+    arr[0].push(i + 1);
+  }
+  let newArray = arr[0].map((item) => item.pow(item));
+  console.log(arr);
+  console.log(newArray);
+  //  arr.map each value * index + 1
+  //  concat arrays
+
+  // let arr = [
+  //   [1, 2, 3],
+  //   [2, 4, 6],
+  //   [3, 6, 9],
+  // ];
+  // console.log(arr[0][1]);
 };
+
+multiplicationTable(3);
