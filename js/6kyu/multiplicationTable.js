17f186c8951cb4a7524b295b2937ef47e6a7e04c
Dan Shawl <danielrshawl@gmail.com>
2022-10-17 20:28:23 -0400

    multiplicationTable solutions

--- js/6kyu/multiplicationTable.js
+++ js/6kyu/multiplicationTable.js
@@ -7,27 +7,50 @@
 // 3 6 9
 // for given example, the return value should be: [[1,2,3],[2,4,6],[3,6,9]]
 
+// let arr = [
+//   [1, 2, 3],
+//   [2, 4, 6],
+//   [3, 6, 9],
+// ];
+
+//  item = arr[0][i] * (i + 1)
+//  push item to new array
+//  repeat for size n
+
+//  we have the first elements of each array
+//  each following element is the i + 1 *
 multiplicationTable = function (size) {
-  //  item = arr[0][i] * (i + 1)
-  //  push item to new array
-  //  repeat for size n
-
-  let arr = [[]];
+  let arr = [];
   for (let i = 0; i < size; i++) {
-    arr[0].push(i + 1);
+    arr.push([i + 1]);
+    for (let j = 0; j < size - 1; j++) {
+      arr[i].push(arr[i][0] * (j + 2));
+    }
   }
-  let newArray = arr[0].map((item) => item.pow(item));
-  console.log(arr);
-  console.log(newArray);
-  //  arr.map each value * index + 1
-  //  concat arrays
-
-  // let arr = [
-  //   [1, 2, 3],
-  //   [2, 4, 6],
-  //   [3, 6, 9],
-  // ];
-  // console.log(arr[0][1]);
+  return arr;
 };
 
-multiplicationTable(3);
+//  Other Solutions
+// multiplicationTable = function(size) {
+//   var result = [];
+
+//   for (var i = 0; i < size; i++) {
+//     result[i] = [];
+//     for(var j = 0; j < size; j++) {
+//       result[i][j] = (i + 1) * (j + 1);
+//     }
+//   }
+
+//   return result
+// }
+
+// multiplicationTable = function(size) {
+
+//   return Array.apply(null, new Array(size)).map(function(val, i) {
+//     return Array.apply(null, new Array(size)).map(function(val, j) {
+//       return (i+1) * (j+1);
+//     });
+//   });
+// }
+
+multiplicationTable(5);
