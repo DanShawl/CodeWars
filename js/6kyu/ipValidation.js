8e974090d69b226b2fff89dc69a425ec147aad26
Dan Shawl <danielrshawl@gmail.com>
2022-10-24 23:27:04 -0400

    more ipValidation

--- js/6kyu/ipValidation.js
+++ js/6kyu/ipValidation.js
@@ -13,6 +13,23 @@
 // Leading zeros (e.g. 01.02.03.04) are considered invalid
 // Inputs are guaranteed to be a single string
 
+//  Check for four octets | split() and length
+//  check each octet for number (0-255) | if its ! >== 0 or <== 255
+//  check for leading 0s | startswith
+
 function isValidIP(str) {
-  return false;
+  str = str.split('.');
+  if (str.length !== 4) return false;
+  for (let i = 0; i < str.length; i++) {
+    console.log(str[i]);
+    if (str[i].startsWith('0')) return false;
+    if (Number(str[i]) < 0 || Number(str[i]) > 255) return false;
+  }
+  return true;
 }
+
+console.log(isValidIP('023.455.67.89'));
+
+// str.forEach((octet) => {
+//   if (Number(octet) < 0 || Number(octet) > 255) return false;
+// });
